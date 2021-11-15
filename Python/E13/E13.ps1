d6df075a2be0db4c36b6270d253abf85c3c72696
OdalysMendoza <89678219+OdalysMendoza@users.noreply.github.com>
2021-11-15 12:01:58 -0600

    Add files via upload

---
+++ Python/E13/E13.ps1
@@ -0,0 +1,19 @@
+﻿$Python = "python.exe"
+
+
+$Script = "C:\Users\odaly\Downloads\BasicOne.py"
+
+
+function Ver-StatusPerfil{  
+    param([Parameter(Mandatory)] [ValidateSet("Public","Private")] [string] $perfil)  
+    $status = Get-NetFirewallProfile -Name $perfil  
+    Write-Host "Perfil:" $perfil  
+    if($status.enabled){  
+        Write-Host "Status: Activado"  
+    } else{  
+        Write-Host "Status: Desactivado"  
+    }  
+}  
+
+
+Ver-StatusPerfil | & $Python $Script
